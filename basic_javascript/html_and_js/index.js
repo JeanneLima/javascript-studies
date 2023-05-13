c13bee33ae9185f746b11cac89d5275aa95d8fa8
Jeanne Lima <jeannelima.profissional@gmail.com>
2023-05-12 22:42:26 -0300

    feat(basic javascript): register rules and examples of browser window object methods

--- basic_javascript/html_and_js/index.js
+++ basic_javascript/html_and_js/index.js
@@ -1,1 +1,17 @@
 console.log('Trecho de código importado.')
+
+/**
+ * Métodos do objeto window do navegador
+ * Seus métodos podem ser acessados sem atribuir "window." no início, como um atalho
+ * alert() exibe uma mensagem em popup nativo do navegador que ao clicar no botão retorna undefined
+ * confirm() exibe uma mensagem em popup nativo do navegador contendo dois botões que retornam valor booleano conforme seleção do usuário
+ * prompt() exibe um popup nativo do navegador com uma caixa de texto e dois botões retornando o valor digitado como string (caso aperte em cancelar, retorna null; se no OK com o campo vazio, retora '' empty string)
+ */
+alert('Mensagem'); // window.alert('Mensagem')
+
+const shouldConfirm = confirm('Deseja realmente apagar?'); // window.confirm('Mensagem')
+console.log('shouldConfirm: ', shouldConfirm)
+
+const myName = prompt('Digite seu nome:'); // window.prompt('Mensagem')
+
+console.log('myName: ', myName)
